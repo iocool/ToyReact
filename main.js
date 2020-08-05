b6d9da8453e8ee9f96454a02d5fd74108a101e6f
iocool <fannypanther@gmail.com>
2020-08-05 19:51:15 +0800

    Day 3 - 虚拟Dom的原理和关键实现

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 import {ToyReact, Component} from './ToyReact'
 
 class Square extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     value: null
+  //   }
+  // }
   render() {
     return (
       <button className='square' onclick={
-        () => this.setState({value: 'X'})
+        this.props.onClick
       }>
-        {this.state.value ? this.state.value : ''}
+        {this.props.value}
       </button>
     )
   }
@@ -20,8 +20,14 @@ class Square extends Component {
 
 class Board extends Component {
   renderSquare(i) {
-    return <Square value={i} />
+    return <Square
+      value={this.props.squares[i]}
+      onClick={
+        () => this.props.onCLick(i)
+      }
+    />
   }
+
   render() {
     return (
       <div>
@@ -45,9 +51,107 @@ class Board extends Component {
   }
 }
 
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+    const winner = calculateWinner(current.squares)
+
+    const move = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={
+            () => this.jumpTo(move)
+          }>{desc}</button>
+        </li>
+      )
+    })
+
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
+
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board
+            squares = {current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
 let a = <Board />
 
 ToyReact.render(
-  a,
+  <Game />,
   document.body
 )
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
